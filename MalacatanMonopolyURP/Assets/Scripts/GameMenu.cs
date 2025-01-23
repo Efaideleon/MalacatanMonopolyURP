25da8641f2c4f5087e75c0d1ebf701c7a1bc21a6
Efaideleon <efaideleon@gmail.com>
2025-01-23 15:18:50 -0800

    menu

--- MalacatanMonopolyURP/Assets/Scripts/GameMenu.cs
+++ MalacatanMonopolyURP/Assets/Scripts/GameMenu.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class GameMenu: MonoBehaviour
@@ -17,12 +16,8 @@ public class GameMenu: MonoBehaviour
     private int _numOfPlayers = 0;
     private string _chosenCharacterName = "";
     private int _currentPlayerChoosingCharacter = 1;
-    private List<string> _playerCharactersPicked = new List<string>();
-
-    public void HandleStartButton()
-    {
-        ChangeMenu(StartMenu,NumOfPlayersMenu);
-    }
+    private readonly List<string> _playerCharactersPicked = new ();
+    private int _numOfRounds = 0;
 
     public void PickNumOfPlayers(int numOfPlayers)
     {
@@ -34,29 +29,74 @@ public class GameMenu: MonoBehaviour
         _chosenCharacterName = characterNameStr;
     }
 
+    public void PickNumberOfRounds(int numOfRounds)
+    {
+        _numOfRounds = numOfRounds;
+    }
+
+    public void HandleStartButton()
+    {
+        ChangeMenu(StartMenu,NumOfPlayersMenu);
+    }
+
     public void HandleConfirmCharacterSelection()
     {
-        Debug.Log($"Player {_currentPlayerChoosingCharacter} picked: {_chosenCharacterName}");
-        _playerCharactersPicked.Add(_chosenCharacterName);
-
-        Debug.Log(_currentPlayerChoosingCharacter + " " + _numOfPlayers);
-        if (_currentPlayerChoosingCharacter == _numOfPlayers)
+        if (_chosenCharacterName != "")
         {
-            // After finishing picking characters for all players go to num of rounds menu.
-            ChangeMenu(CharacterSelectMenu, NumOfRoundsMenu);
+            Debug.Log($"Player {_currentPlayerChoosingCharacter} picked: {_chosenCharacterName}");
+            _playerCharactersPicked.Add(_chosenCharacterName);
+
+            if (_currentPlayerChoosingCharacter == _numOfPlayers)
+            {
+                // After finishing picking characters for all players go to num of rounds menu.
+                ChangeMenu(CharacterSelectMenu, NumOfRoundsMenu);
+            }
+
+            _currentPlayerChoosingCharacter++;
+            _currentPlayerChoosingCharacterText.text = _currentPlayerChoosingCharacter.ToString();
+            _chosenCharacterName = "";
         }
-        _currentPlayerChoosingCharacter++;
-        _currentPlayerChoosingCharacterText.text = _currentPlayerChoosingCharacter.ToString();
+        else
+        {
+            Debug.LogWarning("Character has not been picked!");
+        }
     }
 
     public void HandleConfirmNumOfPlayersButton()
     {
         Debug.Log($"Num of player picked: {_numOfPlayers}");
-        ChangeMenu(NumOfPlayersMenu,CharacterSelectMenu);
+        // TODO: Make sure that the number has been picked before going to next menu.
+        if (_numOfPlayers > 0)
+        {
+            ChangeMenu(NumOfPlayersMenu,CharacterSelectMenu);
+        }
+        else
+        {
+            Debug.LogWarning("Number of players has not been picked!");
+        }   
     }
 
-    public void ChangeMenu(GameObject currentMenu, GameObject nextMenu)
+    public void HandleStartButtonAfterNumOfRounds()
     {
+        Debug.Log($"Num of rounds picked: {_numOfRounds}");
+        if (_numOfRounds > 0) 
+        {
+            Debug.Log("Starting the game...");
+            gameObject.SetActive(false);
+            if (NumOfRoundsMenu != null)
+            {
+                NumOfRoundsMenu.SetActive(false);
+            }
+            else
+            {
+                Debug.LogWarning("Current menu is not assigned!");
+            }
+        }
+    }
+
+    private void ChangeMenu(GameObject currentMenu, GameObject nextMenu)
+    {
+        
         if (currentMenu != null)
         {
             currentMenu.SetActive(false);
