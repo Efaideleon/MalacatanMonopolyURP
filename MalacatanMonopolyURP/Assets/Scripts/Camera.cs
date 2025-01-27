76955a29fb065bba1866567c982f44a6dac8022c
Efaideleon <efaideleon@gmail.com>
2025-01-27 10:45:43 -0800

    camera follow player

---
+++ MalacatanMonopolyURP/Assets/Scripts/Camera.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private GameLogic _gameLogic;
+    private UnityEngine.Camera _camera;
+
+    void Start()
+    {
+        _camera = GetComponent<UnityEngine.Camera>();
+    }
+
+    void OnEnable()
+    {
+        _gameLogic.OnPlayersQueueFilled += HandleGameStart;
+        // When the player rolls the dice, make the camera look at that current player on their new position.
+        _gameLogic.OnDiceRolled += UpdateCameraPosition;
+        // When the player turn ends, make the camera look at the next player.
+        _gameLogic.OnPlayerTurnEnded += UpdateCameraPosition;
+    }
+
+    void OnDisable()
+    {
+        _gameLogic.OnPlayersQueueFilled -= HandleGameStart;
+        _gameLogic.OnDiceRolled -= UpdateCameraPosition;
+        _gameLogic.OnPlayerTurnEnded -= UpdateCameraPosition;
+    }
+
+    private void HandleGameStart()
+    {}
+
+    // Make the camera look at the current active player.
+    private void UpdateCameraPosition()
+    {
+        if (_gameLogic.CurrentActivePlayer)
+        {
+            var offset = new Vector3(-20, 15, 5);
+            transform.position = _gameLogic.CurrentActivePlayer.transform.position + offset;
+            _camera.transform.LookAt(_gameLogic.CurrentActivePlayer.transform);
+        }
+    }
+}
